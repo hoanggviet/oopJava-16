6d203906b314217e29e992cc6a01b39dbf15baa1
Nguyễn Việt <vietnguyen8903@gmail.com>
2023-04-15 06:46:57 +0700

    Baithuchanh ngay 1404

--- BaiThucHanhLab3/src/App.java
+++ BaiThucHanhLab3/src/App.java
@@ -6,15 +6,34 @@ public class App {
         ht.hienthithongtin();
 
         // Hinh tru tron
-        Hinhtrutron htt = new Hinhtrutron();
-        htt.nhapbankinhhtt();
-        htt.nhapchieucao();
-        htt.thongtinhinhtrutron();
+        // Hinhtrutron htt = new Hinhtrutron();
+        // htt.nhapbankinhhtt();
+        // htt.nhapchieucao();
+        // htt.thongtinhinhtrutron();
 
-        // Nhanvien
-        Nhanvien nv = new Nhanvien();
-        nv.nhapthongtin();
-        nv.tinhluong();
-        nv.hienthithongtinnv();
+        // // Nhanvien
+        // Nhanvien nv = new Nhanvien();
+        // nv.nhapthongtin();
+        // nv.tinhluong();
+        // nv.hienthithongtinnv();
     }
 }
+
+// tao class hinh tron có 4 thuoc tinh bkinh, chuvi, dientich, pi;
+
+// 4 phưogn thức 
+// khai bao thuoc tinh la hằng số (// final float PI = 3.14f;) 
+// -1 nhập bán kính 
+//  2 tính chu vi
+//  3 tính diện thích
+//  4 hiển thị chu vi ht
+
+ //  xd class hình trụ tròn ( tương tự hình tròn bổ sung thêm thuộc tính chiều cao và phương thức tính thể tích  hình tròn)
+
+// bafi 3 xây dựng class nhanvien có các thuộc tính sau 
+// 1 hotennv
+// 2 tuoi
+// 3 hesoluong
+// 4 luongcoban = 1490000
+// final int luongcoban = 1490000;
+// cac phuwogn thusc : nhap thông tin nv, tính lương, và hiển thị thông tin nv.
